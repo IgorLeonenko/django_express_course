83606ada9f4e0a63ee82f927de63e8748981ded0
Igor Leonenko <igrleon@gmail.com>
2024-02-09 15:02:03 +0100

    Add CRUD functionality

--- food/urls.py
+++ food/urls.py
@@ -6,4 +6,7 @@ app_name = 'food'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:item_id>/', views.detail, name='detail'),
+    path('add', views.create_item, name='create_item'),
+    path('edit/<int:item_id>', views.edit_item, name='edit_item'),
+    path('delete/<int:item_id>', views.delete_item, name='delete_item'),
 ]
--- food/views.py
+++ food/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Item
+from .forms import ItemForm
 
 def index(request):
     items = Item.objects.all()
@@ -16,3 +16,29 @@ def detail(request, item_id):
         "item": item,
     }
     return render(request, 'food/detail.html', context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    return render(request, 'food/item-form.html', {'form': form})
+
+def edit_item(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    form = ItemForm(request.POST or None, instance=item)
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    context = {
+        'form': form,
+        'item': item
+    }
+    return render(request, 'food/item-form.html', context)
+
+def delete_item(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    return render(request, 'food/item-delete.html', {'item': item})
